578dce075fb43bc409da0a2402875b966f1fc4c2
Henri Bergius <henri.bergius@iki.fi>
2020-12-30 14:23:59 +0100

    Make linter happy

--- components/FetchFeed.js
+++ components/FetchFeed.js
@@ -1,68 +1,68 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS205: Consider reworking code to avoid use of IIFEs
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const noflo = require('noflo');
-const feedparser = require('feedparser');
+const Feedparser = require('feedparser');
 const request = require('request');
 
 // @runtime noflo-nodejs
 
-const handleRss = function (url, res, output) {
-  const parser = new feedparser();
+function handleRss(url, res, output) {
+  const parser = new Feedparser();
   res.pipe(parser);
   parser.once('error', (err) => output.sendDone(err));
-  parser.once('readable', () => output.send({ out: new noflo.IP('openBracket', url) }));
+  parser.once('readable', () => output.send({
+    out: new noflo.IP('openBracket', url),
+  }));
   parser.on('readable', function () {
-    return (() => {
-      let item;
-      const result = [];
-      while ((item = this.read())) {
-        result.push(output.send({ out: item }));
-      }
-      return result;
-    })();
+    let item;
+    const result = [];
+    while (item = this.read()) { // eslint-disable-line no-cond-assign
+      result.push(output.send({ out: item }));
+    }
+    return result;
   });
-  return parser.on('end', () => {
+  parser.on('end', () => {
     output.send({ out: new noflo.IP('closeBracket', url) });
-    return output.done();
+    output.done();
   });
-};
+}
 
-const handleJson = function (url, res, output) {
+function handleJson(url, res, output) {
   let data = '';
-  res.on('data', (chunk) => data += chunk);
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
   return res.on('end', () => {
     let feed;
     try {
       feed = JSON.parse(data);
     } catch (e) {
-      return output.done(e);
+      output.done(e);
+      return;
     }
     if (!(feed.items != null ? feed.items.length : undefined)) {
-      return output.done(new Error(`Invalid JSON feed for ${url}`));
+      output.done(new Error(`Invalid JSON feed for ${url}`));
+      return;
     }
     output.send({ out: new noflo.IP('openBracket', url) });
     const feedMeta = {};
-    for (const key in feed) {
+    Object.keys(feed).forEach((key) => {
       const val = feed[key];
-      if (key === 'items') { continue; }
+      if (key === 'items') { return; }
       feedMeta[key] = val;
-    }
-    for (const item of Array.from(feed.items)) {
-      item.meta = feedMeta;
-      output.send({ out: item });
-    }
+    });
+    feed.items.forEach((item) => {
+      output.send({
+        out: {
+          ...item,
+          meta: feedMeta,
+        },
+      });
+    });
     output.send({ out: new noflo.IP('closeBracket', url) });
-    return output.done();
+    output.done();
   });
-};
+}
 
-exports.getComponent = function () {
+exports.getComponent = () => {
   const c = new noflo.Component();
   c.description = 'Fetch and parse an RSS feed';
   c.icon = 'rss';
@@ -82,10 +82,12 @@ exports.getComponent = function () {
     if (!input.hasData('in')) { return; }
     const data = input.getData('in');
     const req = request(data);
-    req.once('error', (err) => output.sendDone(err));
-    return req.on('response', function (res) {
+    req.once('error', (err) => {
+      output.sendDone(err);
+    });
+    req.on('response', function (res) {
       if (res.statusCode !== 200) {
-        this.emit('error', new Error(`Feed '${data}' resulted in ${res.statusCode}`));
+        output.done(new Error(`Feed '${data}' resulted in ${res.statusCode}`));
         return;
       }
       if ((res.headers['content-type'] != null ? res.headers['content-type'].indexOf('application/json') : undefined) !== -1) {
@@ -93,7 +95,7 @@ exports.getComponent = function () {
         handleJson(data, this, output);
         return;
       }
-      return handleRss(data, this, output);
+      handleRss(data, this, output);
     });
   });
 };
--- spec/FetchFeed.js
+++ spec/FetchFeed.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const chai = require('chai');
 const noflo = require('noflo');
 const path = require('path');
@@ -17,25 +12,28 @@ describe('Feed fetching', () => {
   before(function (done) {
     this.timeout(8000);
     const loader = new noflo.ComponentLoader(baseDir);
-    return loader.load('rss/FetchFeed', (err, instance) => {
-      if (err) { return done(err); }
+    loader.load('rss/FetchFeed', (err, instance) => {
+      if (err) {
+        done(err);
+        return;
+      }
       c = instance;
       ins = noflo.internalSocket.createSocket();
       c.inPorts.in.attach(ins);
-      return done();
+      done();
     });
   });
   beforeEach(() => {
     out = noflo.internalSocket.createSocket();
     error = noflo.internalSocket.createSocket();
     c.outPorts.out.attach(out);
-    return c.outPorts.error.attach(error);
+    c.outPorts.error.attach(error);
   });
   afterEach(() => {
     c.outPorts.out.detach(out);
     c.outPorts.error.detach(error);
     out = null;
-    return error = null;
+    error = null;
   });
 
   describe('fetching a known RSS feed', () => it('should produce 10 items', (done) => {
@@ -58,17 +56,17 @@ describe('Feed fetching', () => {
     out.on('data', (data) => {
       chai.expect(data.meta).to.be.an('object');
       chai.expect(data.meta['rss:link']['#']).to.equal('https://bergie.iki.fi/');
-      return received.push('ITEM');
+      received.push('ITEM');
     });
-    out.on('endgroup', (group) => received.push('>'));
+    out.on('endgroup', () => received.push('>'));
     out.on('disconnect', () => {
       chai.expect(received).to.eql(expected);
-      return done();
+      done();
     });
     error.on('data', (data) => done(data));
 
     ins.send('https://bergie.iki.fi/blog/rss.xml');
-    return ins.disconnect();
+    ins.disconnect();
   }));
   describe('fetching a known JSON feed', () => it('should produce 10 items', (done) => {
     const expected = [
@@ -90,17 +88,17 @@ describe('Feed fetching', () => {
     out.on('data', (data) => {
       chai.expect(data.meta).to.be.an('object');
       chai.expect(data.meta.home_page_url).to.equal('https://bergie.iki.fi');
-      return received.push('ITEM');
+      received.push('ITEM');
     });
-    out.on('endgroup', (group) => received.push('>'));
+    out.on('endgroup', () => received.push('>'));
     out.on('disconnect', () => {
       chai.expect(received).to.eql(expected);
-      return done();
+      done();
     });
     error.on('data', (data) => done(data));
 
     ins.send('https://bergie.iki.fi/blog/feed.json');
-    return ins.disconnect();
+    ins.disconnect();
   }));
 
   describe('fetching a known missing feed', () => it('should produce an error', (done) => {
@@ -111,19 +109,19 @@ describe('Feed fetching', () => {
     error.on('begingroup', (group) => received.push(`< ${group}`));
     error.on('data', (data) => {
       chai.expect(data).to.be.an('error');
-      return received.push('ERR');
+      received.push('ERR');
     });
-    error.on('endgroup', (group) => received.push('>'));
+    error.on('endgroup', () => received.push('>'));
     error.on('disconnect', () => {
       chai.expect(received).to.eql(expected);
-      return done();
+      done();
     });
 
     ins.send('https://bergie.iki.fi/notfound.xml');
-    return ins.disconnect();
+    ins.disconnect();
   }));
 
-  return describe('fetching a non-feed URL', () => it('should produce an error', (done) => {
+  describe('fetching a non-feed URL', () => it('should produce an error', (done) => {
     const received = [];
     const expected = [
       'ERR',
@@ -131,15 +129,15 @@ describe('Feed fetching', () => {
     error.on('begingroup', (group) => received.push(`< ${group}`));
     error.on('data', (data) => {
       chai.expect(data).to.be.an('error');
-      return received.push('ERR');
+      received.push('ERR');
     });
-    error.on('endgroup', (group) => received.push('>'));
+    error.on('endgroup', () => received.push('>'));
     error.on('disconnect', () => {
       chai.expect(received).to.eql(expected);
-      return done();
+      done();
     });
 
     ins.send('https://bergie.iki.fi/blog/');
-    return ins.disconnect();
+    ins.disconnect();
   }));
 });
